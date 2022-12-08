f5b9dadce290509352f9a407024d392f3fe231c4
Cold-Egg <eleu@litespeedtech.com>
2022-12-08 17:47:23 +0800

    modified:   template/entrypoint.sh

--- template/entrypoint.sh
+++ template/entrypoint.sh
@@ -5,6 +5,10 @@ if [ -z "$(ls -A -- "${LSDIR}/conf/")" ]; then
 	cp -R ${LSDIR}/.conf/* ${LSDIR}/conf/
 fi
 
+if [ -z "$(ls -A -- "${LSDIR}/admin/conf/")" ]; then
+	cp -R ${LSDIR}/admin/.conf/* ${LSDIR}/admin/conf/
+fi
+
 if [ ! -e ${LSDIR}/conf/serial.no ] && [ ! -e ${LSDIR}/conf/license.key ]; then
     rm -f ${LSDIR}/conf/trial.key*
     wget -P ${LSDIR}/conf/ http://license.litespeedtech.com/reseller/trial.key
@@ -12,7 +16,7 @@ fi
 chown 999:999 ${LSDIR}/conf/ -R
 chown 999:1000 ${LSDIR}/admin/conf/ -R
 
-/usr/local/lslb/bin/lslbctrl start
+${LSDIR}/bin/lslbctrl start
 $@
 while true; do
 	if ! ${LSDIR}/bin/lslbctrl status | /usr/bin/grep -I 'lslbd is running with PID *' > /dev/null; then
